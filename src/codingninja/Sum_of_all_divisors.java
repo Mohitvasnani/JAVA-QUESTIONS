cd6859cca1d044bd016330d21b5c26c21977b737
Mohitvasnani <mohitvasnani21@gmail.com>
2024-09-07 17:47:12 +0530

    first commit

---
+++ src/codingninja/Sum_of_all_divisors.java
@@ -0,0 +1,19 @@
+package codingninja;
+
+public class Sum_of_all_divisors {
+    public static void main(String[] args) {
+        System.out.println(sumOfAllDivisors(8));
+    }
+
+    public static int sumOfAllDivisors(int n) {
+        int totalSum = 0;
+
+        // Loop over every number i from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Add i to the total sum for each multiple of i
+            totalSum += (n / i) * i;
+        }
+
+        return totalSum;
+    }
+}
