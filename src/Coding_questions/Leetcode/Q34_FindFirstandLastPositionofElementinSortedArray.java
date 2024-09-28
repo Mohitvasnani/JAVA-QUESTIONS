c15dce1c704beca526409da13fda01a99105d660
Mohitvasnani <mohitvasnani21@gmail.com>
2024-09-28 17:22:43 +0530

    commits

--- src/Coding_questions/Leetcode/Q34_FindFirstandLastPositionofElementinSortedArray.java
+++ src/Coding_questions/Leetcode/Q34_FindFirstandLastPositionofElementinSortedArray.java
@@ -5,8 +5,36 @@ import java.util.Arrays;
 public class Q34_FindFirstandLastPositionofElementinSortedArray {
     public static void main(String[] args) {
         int[] arr={5,7,7,8,8,10};
-
         System.out.println(Arrays.toString(BruteForce(arr,8)));
+    }
+    static int[] searchRange(int[] nums, int target){
+        int[] result = {-1,-1};
+        result[0] = search(nums, target,true);
+        if (result[0] != -1) {
+            result[1] = search(nums, 8, false);
+        }
+        return result;
+    }
+    static int search(int[] arr, int target, boolean FindIndex){
+        int ans =-1;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if (arr[mid]>target){
+                end = mid-1;
+            } else if (arr[mid]<target){
+                start = mid+1;
+            } else {
+                ans = mid;
+                if (FindIndex){
+                    end = mid-1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
     }
     static int[] BruteForce(int[] nums, int target) {
         int[] result = {-1,-1};
