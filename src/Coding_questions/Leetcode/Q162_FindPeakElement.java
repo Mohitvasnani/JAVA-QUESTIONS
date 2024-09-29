9207b87319ff8e61b2e65e5881a4ce6091921e8d
Mohitvasnani <mohitvasnani21@gmail.com>
2024-09-29 18:21:23 +0530

    commits

--- src/Coding_questions/Leetcode/Q162_FindPeakElement.java
+++ src/Coding_questions/Leetcode/Q162_FindPeakElement.java
@@ -3,8 +3,39 @@ package Coding_questions.Leetcode;
 public class Q162_FindPeakElement {
     public static void main(String[] args) {
         int[] nums = {1, 2, 1, 3, 5, 6, 4};
-        System.out.println(findPeakElement(nums));
+        System.out.println(Find(nums, 4));
 
+    }
+    static int Find(int[] arr, int target){
+        int peak = findPeakElement(arr);
+        int FirstHalf = AgnosticBinary(arr, target, 0, peak);
+        if (FirstHalf!=-1){
+            return FirstHalf;
+        }
+        return AgnosticBinary(arr, target, peak+1, arr.length-1);
+    }
+    static int AgnosticBinary(int[] arr, int target, int start, int end){
+        boolean IsAscending = arr[end]> arr[start];
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if (arr[mid]==target){
+                return mid;
+            }
+            if (IsAscending){
+                if (target>arr[mid]){
+                    start = mid +1;
+                } else {
+                    end = mid -1;
+                }
+            } else {
+                if (target<arr[mid]){
+                    start = mid+1;
+                } else {
+                    end = mid -1;
+                }
+            }
+        }
+        return -1;
     }
     static int findPeakElement(int[] nums) {
         int start =0;
--- src/Kunal_kushwaha/Binary_search/OrderAgnosticSearch.java
+++ src/Kunal_kushwaha/Binary_search/OrderAgnosticSearch.java
@@ -6,14 +6,10 @@ public class OrderAgnosticSearch {
         int[] ascendingArray = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
         System.out.println(search(ascendingArray, 10));
         System.out.println(search(descendingArray, 5));
-
-
-
     }
     static int search(int[] arr, int target){
         int start =0;
         int end = arr.length-1;
-
         boolean isAscending = arr[end]>arr[start];
         while(start<=end){
             int mid = start + (end-start)/2;
@@ -32,7 +28,6 @@ public class OrderAgnosticSearch {
                     end = mid -1;
                 }
             }
-
         }
         return -1;
     }
