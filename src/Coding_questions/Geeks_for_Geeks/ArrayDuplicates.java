388d17652268636d2c4dec044a97c553a5baf591
Mohitvasnani <mohitvasnani21@gmail.com>
2024-10-02 17:54:43 +0530

    commits

--- src/Coding_questions/Geeks_for_Geeks/ArrayDuplicates.java
+++ src/Coding_questions/Geeks_for_Geeks/ArrayDuplicates.java
@@ -1,4 +1,27 @@
 package Coding_questions.Geeks_for_Geeks;
 
+import java.util.ArrayList;
+
 public class ArrayDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 1, 2, 3, 5, 6, 5, 2};
+        System.out.println(finddup(arr));
+
+    }
+    static ArrayList<Integer> finddup(int[] arr){
+        ArrayList<Integer> result =  new ArrayList<>();
+        int[] freq = new int[arr.length];
+        for (int i = 0; i <arr.length ; i++) {
+            freq[arr[i]]++;
+        }
+        for (int i = 0; i < freq.length ; i++) {
+            if (freq[i]>1){
+                result.add(i);
+            }
+        }
+        if (result.isEmpty()){
+            result.add(-1);
+        }
+        return result;
+    }
 }
