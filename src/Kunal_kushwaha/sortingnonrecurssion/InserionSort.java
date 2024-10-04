9bf2d522b26af9679850e9f6fc06d0d9d8adc2c9
Mohitvasnani <mohitvasnani21@gmail.com>
2024-10-05 00:02:51 +0530

    today

---
+++ src/Kunal_kushwaha/sortingnonrecurssion/InserionSort.java
@@ -0,0 +1,22 @@
+package Kunal_kushwaha.sortingnonrecurssion;
+import java.util.Arrays;
+
+import static Kunal_kushwaha.sortingnonrecurssion.Swap.swap;
+public class InserionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println(Arrays.toString(insert(arr)));
+    }
+    static int[] insert(int[] arr){
+        for (int i = 0; i < arr.length-1 ; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/Kunal_kushwaha/sortingnonrecurssion/Swap.java
@@ -0,0 +1,11 @@
+package Kunal_kushwaha.sortingnonrecurssion;
+
+public class Swap {
+    public static int[] swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]= temp;
+        return arr;
+    }
+
+}
