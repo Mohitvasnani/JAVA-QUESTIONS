ebf32bdc3cc677face57e6b4888f1731b0ba4474
Mohitvasnani <mohitvasnani21@gmail.com>
2024-10-05 13:44:10 +0530

    today 2nd

---
+++ src/Kunal_kushwaha/sortingnonrecurssion/Cyclesort.java
@@ -0,0 +1,24 @@
+package Kunal_kushwaha.sortingnonrecurssion;
+import java.util.Arrays;
+
+import static Kunal_kushwaha.sortingnonrecurssion.Swap.swap;
+
+public class Cyclesort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,1,2};
+        System.out.println(Arrays.toString(cycle(arr)));
+    }
+    static int[] cycle(int[] arr){
+        int i =0;
+        while (i< arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        return arr;
+    }
+
+}
