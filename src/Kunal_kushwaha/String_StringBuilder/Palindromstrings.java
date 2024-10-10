de213edf7637310bb2e2e30e25c68fcd57eaf9bc
Mohitvasnani <mohitvasnani21@gmail.com>
2024-10-10 22:56:57 +0530

    commits

--- src/Kunal_kushwaha/String_StringBuilder/Palindromstrings.java
+++ src/Kunal_kushwaha/String_StringBuilder/Palindromstrings.java
@@ -2,14 +2,26 @@ package Kunal_kushwaha.String_StringBuilder;
 
 public class Palindromstrings {
     public static void main(String[] args) {
-        String str = "abcdca";
-        System.out.println(IsPalindrome(str));
+        String str = "abcdcba";
+        System.out.println(Onlystring(str));
 
     }
     static boolean IsPalindrome(String str){
          StringBuffer sb = new StringBuffer(str);
          sb.reverse();
-
         return sb.toString().equals(str);
+    }
+    static boolean Onlystring(String str){
+        char[] arr = str.toCharArray();
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            if (arr[start]!=arr[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 }
